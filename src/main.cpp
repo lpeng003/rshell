b38584ab091fe07f8c833f478a8f2ac9cf47b863
Leon Peng <lpeng003@ucr.edu>
2014-10-20 20:35:36 -0700

    Added strtok to parse commands

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,28 @@ int main(int argc, char* argv[])
 	int pid = fork();
 	if(pid == 0)//when pid is 0, this is the child process
 	{
-		cout << "This is the child.";
+		//cout << "This is the child." << endl;
+		for(int x = 0; x< argc; ++x)
+		{
+			char* str = argv[x];
+			char * tok = strtok(str," ;|&");
+			while(tok != NULL)
+			{
+				cout << tok << endl;
+				tok = strtok(NULL,"; |&");
+			}
+		}
 		exit(1);
 	}
 	else if(pid > 0)//When pid > 0, this is the parent process
 	{
 		if(-1 == wait(0))
 		perror("There was a problem with wait");
-		cout << "This is the parent.";
-	}	
+		//cout << "This is the parent." << endl;
+	}
+	else if(pid == -1)
+	{
+		perror("There is a problem with fork()");
+	}
+	return 0;	
 }
