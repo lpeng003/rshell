178886f8c73b463185fdb05cf86d7d44de9b7125
Leon Peng <lpeng003@hammer.cs.ucr.edu>
2014-11-10 03:29:26 -0800

    Implemented connectors

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -11,6 +11,53 @@
 
 using namespace std;
 
+bool exec_vp(string commands)
+{
+	const int y = commands.size() + 1;
+	char* stuff = new char[y];//Stores command line as c strings
+	strcpy(stuff,commands.c_str());
+
+	char** cmd= new char*[y];//Stores the parsed command line
+	int x = 0;
+	char* tok = strtok(stuff, " \n\t\r");//Parses the c_strings
+
+	while(tok != NULL)//Adding tokens into storage
+	{
+		if(strcmp(tok, "exit") == 0)//if an argument is exit
+					    //it will exit
+		{
+			exit(0);
+		}
+		cmd[x] = tok;
+		tok = strtok(NULL," \n\t\r");
+		//cout << cmd[x] << " ";
+		++x;
+	}
+	
+	cmd[x] = NULL;//Makes last argument the null character
+	int pid = fork();
+	if(pid == 0)
+	{
+		//cout << "This is the child" << endl;
+		if(-1 == execvp(cmd[0], cmd))
+		perror("execvp");
+		exit(1);
+	}
+	else if (pid == -1)//If fork fails
+	{
+		perror("fork");
+	}
+	else
+	{
+		if(-1 == wait(0))
+		{
+			perror("wait");
+		}
+		//cout << "This is the parent" << endl;
+	}
+	return false;
+}
+
 int main()
 {
 	while(true)
@@ -40,52 +87,61 @@ int main()
 		int size_str = commands.find("#");
 		commands = commands.substr(0,size_str);
 
-		const int y = commands.size() + 1;
-		char* stuff = new char[y];//Stores command line as c strings
-		strcpy(stuff,commands.c_str());
-
-		char** cmd= new char*[y];//Stores the parsed command line
-		int x = 0;
-		char* tok = strtok(stuff, " ;\n\t\r");//Parses the c_strings
-
-		while(tok != NULL)//Adding tokens into storage
-		{
-			if(strcmp(tok, "exit") == 0)//if an argument is exit
-						    //it will exit
-			{
-				exit(0);
-			}
-			cmd[x] = tok;
-			tok = strtok(NULL," ;\n\t\r");
-			//cout << cmd[x] << " ";
-			++x;
-		}
 		
-		//cout << x << " " << x+1 << endl;Checks how many arguments and
-						//which would be the null 
-						//argument
-		cmd[x] = NULL;//Makes last argument the null character
-		int pid = fork();
-		if(pid == 0)
-		{
-			//cout << "This is the child" << endl;
-			if(-1 == execvp(cmd[0], cmd))
-			perror("execvp");
-			exit(1);
-		}
-		else if (pid == -1)//If fork fails
-		{
-			perror("fork");
-		}
-		else
-		{
-			if(-1 == wait(0))
-			{
-				perror("wait");
-			}
-			//cout << "This is the parent" << endl;
-		}
 
+		//Seperating by connectors
+		int connect = 0;
+		bool no_connect = true;
+		bool next_it = true;
+		string flag = "";
+		while(no_connect)
+		{
+			if(next_it)
+			{
+				if(commands.find("||")!=string::npos)
+				{
+					connect = commands.find("||");
+					flag = "||";
+				}
+				else if(commands.find("&&")!=string::npos)
+				{
+					connect = commands.find("&&");
+					flag = "&&";
+				}
+				else if(commands.find(";")!=string::npos)
+				{
+					connect = commands.find(";");
+					flag = ";";
+				}
+				else
+				no_connect = false;
+				if(flag == "||")
+				{
+					string curr = commands.substr(0,connect);
+					if(exec_vp(curr)==true)
+					next_it = false;
+					commands = commands.substr(connect+2);
+				}
+				else if(flag == "&&")
+				{
+					string curr = commands.substr(0,connect);
+					if(exec_vp(curr)==false)
+					next_it = false;
+					commands = commands.substr(connect+2);
+				}
+				else if(flag == ";")
+				{
+					string next = commands.substr(0,connect);
+					if(exec_vp(next));
+					commands = commands.substr(connect+1);
+				}
+				else
+					exec_vp(commands);
+				flag = "";
+			}
+			else
+			next_it = true;
+		}
 	}
 
 
